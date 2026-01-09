abaa64fefe37999ffba7fa30a4078104e2d8b2aa
EmmaDirk <156461181+EmmaDirk@users.noreply.github.com>
2026-01-09 14:04:48 +0100

    made a virtual environment for the compendium

--- 00_exercises/04_reproducibility/Ex2_4.4_repro/scripts/00_packages.R
+++ 00_exercises/04_reproducibility/Ex2_4.4_repro/scripts/00_packages.R
@@ -1,16 +1,9 @@
 # this script loads the required packages for this simulation study
-# and installs them if not already installed
+# we do not install packages since they are managed by renv
 # ---------------------------------------------------------------------
 
 # required packages
 pkgs <- c("Hmisc", "mice", "tidyverse", "here")
-
-# install packages if not already installed
-to_install <- pkgs[!pkgs %in% rownames(installed.packages())]
-
-if (length(to_install) > 0) {
-  install.packages(to_install)
-}
 
 # load packages
 lapply(pkgs, library, character.only = TRUE)
